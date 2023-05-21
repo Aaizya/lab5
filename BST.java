a90dfcde96b075ec87de75a62448fec68e3f7935
Aruzhan <aruzhan1471@gmail.com>
2023-05-21 16:48:13 +0600

    3 commit

--- BST.java
+++ BST.java
@@ -1,4 +1,8 @@
-public class BST <K extends Comparable <K>, V> {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Stack;
+
+public class BST<K extends Comparable<K>, V> implements Iterable<BST.KeyValue<K, V>> {
     private Node root;
     private int size;
 
@@ -6,41 +10,42 @@ public class BST <K extends Comparable <K>, V> {
         private K key;
         private V val;
         private Node left, right;
-        private int count;
-
 
         public Node(K key, V val) {
             this.key = key;
             this.val = val;
-            this.count = 1;
         }
     }
 
     public void put(K key, V val) {
-        root = put( root , key, val);
+        root = put(root, key, val);
     }
-    private Node put (Node node, K key, V val) {
-        if ( node == null) {
+
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
             size++;
-            return new Node(key,val);
+            return new Node(key, val);
         }
-        int cmp = key.compareTo (node.key);
-        if (cmp<0) {
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
             node.left = put(node.left, key, val);
-        }
-        else if (cmp>0) {
-            node.right= put(node.right,key,val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
         } else {
             node.val = val;
         }
-        node.count = 1 + size(node.left) + size(node.right);
+
         return node;
     }
+
     public V get(K key) {
+        Node node = get(root, key);
+        return node != null ? node.val : null;
     }
 
-    public void delete(K key) {
-    }
+    private Node get(Node node, K key) {
+        if (node == null) {
+            return null;
+        }
 
-    public Iterable<K> iterator() {}
-}
